e57c147b0173dc5cbc234187688142cdf3968ec0
MRAEIVSEONN <124403172+MRAEIVSEONN@users.noreply.github.com>
2023-02-03 16:34:10 -0500

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+console.log("Hello WORLD");
+//Suma
+var a=15;
+var b=54;
+var r= a+b;
+console.log("La suma es:"+ r);
+//Multiplicacion
+var n=15;
+var m=54;
+var re= n*m;
+console.log("La suma es:"+ re);
+//Raiz Cuadrada
+var raiz = Math.sqrt(1244);
+console.log("La raiz cuadrada de 1244 es:" + raiz );
